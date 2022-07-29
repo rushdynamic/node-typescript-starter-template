860874105dc746976cfd8285c7d4c84b8536d186
RushDynamic <tameimpalafanclubpresident@gmail.com>
2022-07-29 15:09:01 +0530

    fix: clean template's git repo post install

--- bin/cli.js
+++ bin/cli.js
@@ -1,6 +1,15 @@
 #!/usr/bin/env node
-
 const { execSync } = require('child_process');
+const ascii = `
+ ____   __ __   _____ __ __ 
+|    \\ |  |  | / ___/|  |  |
+|  D  )|  |  |(   \\_ |  |  |
+|    / |  |  | \\__  ||  _  |
+|    \\ |  :  | /  \\ ||  |  |
+|  .  \\|     | \\    ||  |  |
+|__|\\_| \\__,_|  \\___||__|__|
+                            `;
+console.log(ascii);
 
 const runCmd = (cmd) => {
     try {
@@ -14,14 +23,22 @@ const runCmd = (cmd) => {
 
 const repoName = process.argv[2];
 const gitCheckoutCmd = `git clone --depth 1 https://github.com/RushDynamic/node-typescript-starter-template ${repoName}`;
+const removeGitDirCmd = `cd ${repoName} && rm -rf .git`;
 const installDepsCmd = `cd ${repoName} && npm install --include=dev`;
 
-console.log(`Cloning repo: ${repoName}`);
+// clone repo
 const gitRes = runCmd(gitCheckoutCmd);
 if (!gitRes) process.exit(-1);
 
+// delete .git dir
+console.log("Removing template's .git directory");
+const removeRes = runCmd(removeGitDirCmd);
+if (!removeRes) process.exit(-1);
+
+// install deps
 console.log('Installing dependencies');
 const depsRes = runCmd(installDepsCmd);
 if (!depsRes) process.exit(-1);
 
-console.log('Setup successful, happy hacking!');
+console.log("Setup successful, be sure to update package.json with your app's  information");
+console.log('Happy hacking.');
